6c37511da2f5f057bfe12e6caba10b33798698f3
Hai Nguyen Dang <haind@sonat.vn>
2023-04-22 14:51:49 +0700

    add popup html

--- js/main.js
+++ js/main.js
@@ -1,81 +1,24 @@
-// // var urlInput = window.location.href
-// // var hostname = new URL(urlInput).hostname
-
-// // function postAIResponse(urlInput) {
-// //   var xhr = new XMLHttpRequest()
-// //   var url = 'https://phishingdetector.azurewebsites.net/getprediction'
-
-// //   xhr.open('GET', url + '?url=' + urlInput, true)
-// //   xhr.setRequestHeader('Content-Type', 'application/json')
-// //   xhr.send()
-
-// //   xhr.onreadystatechange = function () {
-// //     if (xhr.readyState == 4 && xhr.status == 200) {
-// //       var json = JSON.parse(xhr.responseText)['prediction']
-// //       console.log(json)
-// //       response = json == '[1]' ? 'Phish' : 'Legit'
-
-// //       chrome.pageAction.setBadgeText({ text: response })
-// //       chrome.pageAction.setBadgeBackgroundColor({
-// //         color: response === 'Phish' ? '#bc5858' : '#5cb85c',
-// //       })
-// //     }
-// //   }
-// // }
-
-// // postAIResponse(urlInput)
-
-// function postAIResponse(urlInput, callback) {
-//   var xhr = new XMLHttpRequest()
-//   var url = 'https://phishingdetector.azurewebsites.net/getprediction'
-//   xhr.open('GET', url + '?url=' + urlInput, true)
-//   xhr.setRequestHeader('Content-Type', 'application/json')
-//   xhr.send()
-//   xhr.onreadystatechange = function () {
-//     if (xhr.readyState == 4 && xhr.status == 200) {
-//       var json = JSON.parse(xhr.responseText)['prediction']
-//       console.log(json)
-//       if (json == '[1]') {
-//         callback('Phish')
-//       } else {
-//         callback('Legit')
-//       }
-//     }
-//   }
-// }
-
-// let urlInput = window.location.href
-// let hostname = new URL(urlInput).hostname
-
-// postAIResponse(urlInput, function (response) {
-// //   chrome.action.setBadgeText({ text: response })
-// //   chrome.action.setBadgeBackgroundColor({
-// //     color: response === 'Phish' ? '#bc5858' : '#5cb85c',
-// //   })
-
-//     // it is running in the content script
-//     // we have to use document.getElementById   
+// var popup = document.createElement('div');
+// popup.id = 'my-popup';
     
-//     // so we can't use chrome.action.setBadgeText
+// // Set the styles for the popup
+// popup.style.position = 'fixed';
+// popup.style.top = '50%';
+// popup.style.left = '50%';
+// popup.style.transform = 'translate(-50%, -50%)';
+// popup.style.zIndex = '9999';
+// popup.style.width = '400px';
+// popup.style.height = '300px';
+// popup.style.backgroundColor = '#fff';
+// popup.style.border = '1px solid #ccc';
+// popup.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.3)';
 
-// })
+// // Add an event listener to close the popup when the user clicks outside of it
+// document.addEventListener('click', function(event) {
+//   if (event.target.closest('#my-popup') === null) {
+//     popup.remove();
+//   }
+// });
 
-// // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-// //   console.log('tab updated')
-// //   if (changeInfo.status === 'complete') {
-// //     let url = tab.url
-// //     console.log(url)
-// //     // check url is right regex
-// //     if (url.match(/^(http|https):\/\/[^ "]+$/)) {
-// //       let domain = new URL(url).hostname
-// //       postAIResponse(url, function (response) {
-// //         // document.getElementById('res-circle').style.backgroundColor =
-// //         //   response === 'Phish' ? '#bc5858' : '#5cb85c'
-// //         chrome.action.setBadgeText({ text: response })
-// //         chrome.action.setBadgeBackgroundColor({
-// //           color: response === 'Phish' ? '#bc5858' : '#5cb85c',
-// //         })
-// //       })
-// //     }
-// //   }
-// // })
+// // Append the popup to the document
+// document.body.appendChild(popup);
