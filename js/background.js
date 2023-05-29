51198caaecfc3deebe2f2d9f4f9e4247016ea11c
Hai Nguyen Dang <haind@sonat.vn>
2023-05-29 20:21:17 +0700

    fix bug

--- js/background.js
+++ js/background.js
@@ -1,167 +1,173 @@
-const MAX_URLS = 30
-const MAX_DAYS = 5
-var activeTab = 0
-results = []
-let db
+const MAX_URLS = 30;
+const MAX_DAYS = 5;
+var activeTab = 0;
+var results = [];
+let db;
 
 function openDB() {
-  const request = indexedDB.open('myDatabase', 1)
+  const request = indexedDB.open('myDatabase', 1);
 
   request.onerror = function (event) {
-    console.log('Error opening indexedDB')
-  }
+    console.log('Error opening indexedDB');
+  };
 
   request.onsuccess = function (event) {
-    db = event.target.result
-    console.log('IndexedDB connection successful')
-  }
+    db = event.target.result;
+    console.log('IndexedDB connection successful');
+  };
 
   request.onupgradeneeded = function (event) {
-    db = event.target.result
+    db = event.target.result;
 
-    // Tạo object store (bảng) để lưu trữ dữ liệu
+    // Create an object store (table) to store the data
     if (!db.objectStoreNames.contains('reportedURLs')) {
       const objectStore = db.createObjectStore('reportedURLs', {
         keyPath: 'id',
         autoIncrement: true,
-      })
+      });
 
-      // Tạo các chỉ mục (indexes) nếu cần thiết
-      objectStore.createIndex('urlIndex', 'url', { unique: true })
+      // Create any necessary indexes
+      objectStore.createIndex('urlIndex', 'url', { unique: true });
     }
-  }
+  };
 }
 
-openDB()
+openDB();
 
 function postAIResponse(urlInput, callback) {
-  fetch(
-    'https://phishingdetector.azurewebsites.net/getprediction?url=' + urlInput,
-  )
+  fetch('https://phishingdetector.azurewebsites.net/getprediction?url=' + urlInput)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
+      console.log(data);
       if (data.prediction == '[1]') {
-        callback('Phish')
+        callback('Phish');
       } else if (data.prediction == '[0]') {
-        callback('Legit')
+        callback('Legit');
       }
     })
+    .catch((error) => {
+      console.log('Error fetching AI response:', error);
+      callback(null);
+    });
 }
 
 function updateBadge(url) {
-  chrome.action.setBadgeText({ text: '...' })
-  chrome.action.setBadgeBackgroundColor({ color: '#5bc0de' })
+  chrome.action.setBadgeText({ text: '...' });
+  chrome.action.setBadgeBackgroundColor({ color: '#5bc0de' });
 
   checkURLInIndexedDB(url, function (response) {
     if (response) {
-      // URL đã được báo cáo và được xác định là lừa đảo
-      chrome.action.setBadgeText({ text: response })
-      chrome.action.setBadgeBackgroundColor({ color: '#bc5858' })
+      // URL has been reported and identified as phishing
+      chrome.action.setBadgeText({ text: response });
+      chrome.action.setBadgeBackgroundColor({ color: '#bc5858' });
 
-      const result = { url: url, response: response }
-      results.push(result)
+      const result = { url: url, response: response };
+      results.push(result);
 
       if (results.length > MAX_URLS) {
-        results.shift() // remove the oldest URL
+        results.shift(); // remove the oldest URL
       }
 
-      chrome.storage.local.set({ results: results }, function () {})
-      
-
+      chrome.storage.local.set({ results: results }, function () {});
     } else {
       if (url.match(/^(http|https):\/\/[^ "]+$/)) {
         chrome.storage.local.get('results', function (data) {
-          let found = false
+          let found = false;
           if (data.results) {
             for (let i = 0; i < data.results.length; i++) {
               if (data.results[i].url === url) {
-                chrome.action.setBadgeText({ text: data.results[i].response })
+                chrome.action.setBadgeText({ text: data.results[i].response });
                 chrome.action.setBadgeBackgroundColor({
                   color:
-                    data.results[i].response === 'Phish'
-                      ? '#bc5858'
-                      : '#5cb85c',
-                })
-                found = true
-                break
+                    data.results[i].response === 'Phish' ? '#bc5858' : '#5cb85c',
+                });
+                found = true;
+                break;
               }
             }
           }
 
           if (!found) {
             postAIResponse(url, function (response) {
-              chrome.action.setBadgeText({ text: response })
-              chrome.action.setBadgeBackgroundColor({
-                color: response === 'Phish' ? '#bc5858' : '#5cb85c',
-              })
-              const result = { url: url, response: response }
-              results.push(result)
+              if (response) {
+                chrome.action.setBadgeText({ text: response });
+                chrome.action.setBadgeBackgroundColor({
+                  color: response === 'Phish' ? '#bc5858' : '#5cb85c',
+                });
+                const result = { url: url, response: response };
+                results.push(result);
 
-              if (results.length > MAX_URLS) {
-                results.shift() // remove the oldest URL
+                if (results.length > MAX_URLS) {
+                  results.shift(); // remove the oldest URL
+                }
+                chrome.storage.local.set({ results: results }, function () {});
               }
-              chrome.storage.local.set({ results: results }, function () {})
-            })
+            });
           }
-        })
+        });
       } else {
-        return
+        return;
       }
     }
-  })
+  });
 }
 
 chrome.tabs.onActivated.addListener(function (activeInfo) {
-  activeTab = activeInfo.tabId
+  activeTab = activeInfo.tabId;
   chrome.tabs.get(activeInfo.tabId, function (tab) {
-    console.log('OnActivated: ' + tab.url)
-    updateBadge(tab.url)
-  })
-})
+    console.log('OnActivated: ' + tab.url);
+    updateBadge(tab.url);
+  });
+});
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.url && tabId === activeTab) {
-    console.log('OnUpdated: ' + changeInfo.url)
-    updateBadge(tab.url)
+    console.log('OnUpdated: ' + changeInfo.url);
+    updateBadge(tab.url);
   }
-})
+});
 
 function checkURLInIndexedDB(url, callback) {
-  const transaction = db.transaction(['reportedURLs'], 'readonly')
-  const objectStore = transaction.objectStore('reportedURLs')
-  const index = objectStore.index('urlIndex')
+  if (!db) {
+    console.log('IndexedDB is not open');
+    callback(null);
+    return;
+  }
 
-  const request = index.get(url)
+  const transaction = db.transaction(['reportedURLs'], 'readonly');
+  const objectStore = transaction.objectStore('reportedURLs');
+  const index = objectStore.index('urlIndex');
+
+  const request = index.get(url);
 
   request.onsuccess = function (event) {
-    const data = event.target.result
+    const data = event.target.result;
     if (data) {
-      callback('Phish')
+      callback('Phish');
     } else {
-      callback(null) // Không tìm thấy URL trong IndexedDB
+      callback(null); // URL not found in IndexedDB
     }
-  }
+  };
 
   request.onerror = function (event) {
-    console.log('Error checking URL in IndexedDB')
-    callback(null)
-  }
+    console.log('Error checking URL in IndexedDB');
+    callback(null);
+  };
 }
 
 // function cleanupExpiredData() {
 //   chrome.storage.local.get('results', function (data) {
 //     if (data.results) {
-//       const now = Date.now()
-//       const maxAge = MAX_DAYS * 24 * 60 * 60 * 1000 // convert days to milliseconds
+//       const now = Date.now();
+//       const maxAge = MAX_DAYS * 24 * 60 * 60 * 1000; // convert days to milliseconds
 //       const validResults = data.results.filter(
-//         (result) => now - result.timestamp <= maxAge,
-//       )
+//         (result) => now - result.timestamp <= maxAge
+//       );
 
-//       chrome.storage.local.set({ results: validResults }, function () {})
+//       chrome.storage.local.set({ results: validResults }, function () {});
 //     }
-//   })
+//   });
 // }
 
 // // Clean up expired data once a day
-// setInterval(cleanupExpiredData, 24 * 60 * 60 * 1000)
+// setInterval(cleanupExpiredData, 24 * 60 * 60 * 1000);
